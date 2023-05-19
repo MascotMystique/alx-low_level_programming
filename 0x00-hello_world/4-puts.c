88dcab1f0c0e30b18d79fbe32286fe79c19b6006
MascotMystique <mosesadukwu0@gmail.com>
2023-05-18 20:17:43 -0700

    Using puts to print my first c output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * puts - a function that prints out a string of characters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
