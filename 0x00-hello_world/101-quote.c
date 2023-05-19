d37164b554141fd9a3e9548e93b85db04ac371ec
MascotMystique <mosesadukwu0@gmail.com>
2023-05-18 21:29:53 -0700

    Quote to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description - a function/program that prints a string to the standard error
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, err, 59);
+	return(1);
+}
